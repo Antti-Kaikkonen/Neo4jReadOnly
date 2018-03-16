a9fbbfa148cbaac09079b39b5cb9ccf4cc00566b
Antti Kaikkonen <35118741+Antti-Kaikkonen@users.noreply.github.com>
2018-03-16 19:31:32 +0200

    github release

---
+++ run.ts
@@ -0,0 +1,89 @@
+import * as http from 'http';
+import * as request from 'request';
+import * as url from 'url';
+
+let config = require('./config');
+
+let cypher_transaction_url = config.neo4j_transaction_url;
+
+let headers = {'Content-Type': 'application/json',"Access-Control-Allow-Origin" : "*"};
+
+let executeQuery = (statement: string, params: object, cb: (error, response, body) => void): any => {
+	request.post(
+		cypher_transaction_url,
+		{ 
+			json: { 
+				statements: [{statement: statement, includeStats:true, parameters: params}]
+			} 
+		},(error, response, body) => {
+			cb(error, response, body);
+			if (typeof body.commit !== "string") return;
+			let commitURL: string = body.commit;
+			let contains_updates = body.results && body.results.length > 0 && body.results[0].stats.contains_updates;
+			if (!contains_updates) {
+				request.post(commitURL, { json: { statements: []} }).auth(config.neo4j_user, config.neo4j_password, false);
+			} else {
+				request.delete(commitURL).auth(config.neo4j_user, config.neo4j_password, false);;//Rollback (readonly)
+			}
+		}
+	).auth(config.neo4j_user, config.neo4j_password, false);
+};
+
+http.createServer((request, response) => {
+	let query: any = url.parse(request.url, true).query;
+	let path:string = url.parse(request.url).pathname;
+	if (path.indexOf("/db/data/read-only_query") !== 0) {
+		response.writeHead(404, headers);
+		response.end();
+		return;
+	}
+	if (request.method === "POST") {
+		var jsonString = '';
+		request.on('data', function (data) {
+				jsonString += data;
+		});
+		request.on('end', function () {
+				let obj = JSON.parse(jsonString);
+				let query = obj.query;
+				if (typeof query === "string") {
+					executeQuery(query, {}, (error, res, body) => {
+						if (body.errors && body.errors.length > 0) {
+							response.writeHead(400, headers);
+							response.write(JSON.stringify(body.errors[0]));
+							response.end();
+						} else if (body.results && body.results.length > 0) {
+							let results = { ... body.results[0] };
+							let oldData = <[any]>results.data
+							let newData = oldData.map(e => e.row);
+							results.data = newData;
+							delete results.stats;
+							response.writeHead(200, headers);
+							response.write(JSON.stringify(results));
+							response.end();
+						} else {
+							response.writeHead(400, headers);
+							response.end();
+						}
+					});
+				} else {
+					response.writeHead(400, headers);
+					response.end();
+				}
+				
+		});
+	} else if (request.method === 'OPTIONS') {
+		var options_headers = {};
+		// IE8 does not allow domains to be specified, just the *
+		// headers["Access-Control-Allow-Origin"] = req.headers.origin;
+		options_headers["Access-Control-Allow-Origin"] = "*";
+		options_headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+		options_headers["Access-Control-Allow-Credentials"] = false;
+		options_headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+		options_headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+		response.writeHead(200, options_headers);
+		response.end();
+  } else {
+		response.writeHead(400, headers);
+		response.end();
+	}
+}).listen(config.port, config.host);
